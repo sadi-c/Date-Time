6dd2dd2d3b70d03de6cbe2e0c11b19fd0241dcf7
Sadika Chowdhury <sadikachowdhury29@gmail.com>
2021-01-25 00:23:03 -0500

    datetime

---
+++ datetime.py
@@ -0,0 +1,9 @@
+import datetime  
+    
+# using now() to get current time  
+current_time = datetime.datetime.now()  
+    
+# Printing value of now.  
+print ("Time now at greenwich meridian is : "
+                                    , end = "")  
+print (current_time)  
