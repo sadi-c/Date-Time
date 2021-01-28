36a988e2a12ef9e75fb1aaa0f6d31376b08e66dd
Sadika Chowdhury <sadikachowdhury29@gmail.com>
2021-01-27 22:57:17 -0500

    added location

--- datetime.py
+++ datetime.py
@@ -4,6 +4,6 @@ import datetime
 current_time = datetime.datetime.now()  
     
 # Printing value of now.  
-print ("Time now at greenwich meridian is : "
+print ("Time now at California is : "
                                     , end = "")  
 print (current_time)  
